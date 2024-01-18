e4581f8ca48eecf1a2526bbf144a26f54dd70c89
yonggyo1125 <yonggyo00@kakao.com>
2024-01-18 09:36:02 +0900

    상품 상세

--- src/main/resources/static/front/js/product/detail.js
+++ src/main/resources/static/front/js/product/detail.js
@@ -1,3 +1,17 @@
+/**
+* 상품 상세 기능 모음
+*
+*/
+const productDetails = {
+    /**
+    * 구매 수량 변경
+    *
+    */
+    changeEa(el) {
+
+    }
+};
+
 window.addEventListener("DOMContentLoaded", function() {
     /* 상품 메인 썸네일 이벤트 처리 S */
     const thumbs = document.querySelectorAll(".thumbs .thumb");
@@ -12,4 +26,12 @@ window.addEventListener("DOMContentLoaded", function() {
         }
     }
     /* 상품 메인 썸네일 이벤트 처리 E */
+
+    /* 상품 수량 증가, 감소 처리 S */
+    const changeEaEls = document.querySelectorAll(".selected_products .change_ea");
+    for (const el of changeEaEls) {
+        el.addEventListener("click", productDetails.changeEa);
+    }
+    /* 상품 수량 증가, 감소 처리 E */
+
 });
