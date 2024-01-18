1fd0e6b8277ca09d13c2d01589ccf049b92a4556
yonggyo1125 <yonggyo00@kakao.com>
2024-01-18 12:00:31 +0900

    장바구니 처리

---
+++ src/main/java/org/choongang/cart/service/CartData.java
@@ -0,0 +1,16 @@
+package org.choongang.cart.service;
+
+import lombok.Data;
+import org.choongang.cart.entities.CartInfo;
+
+import java.util.List;
+
+@Data
+public class CartData {
+    private List<CartInfo> items; // 장바구니 상품 목록
+
+    private int totalPrice; // 상품가 합계
+    private int totalDiscount; // 할인금액 합계
+    private int totalDeliveryPrice; // 배송비 합계
+    private int payPrice; // 결제금액 합계 - 상품가 - 할인금액 + 배송비
+}
--- src/main/java/org/choongang/cart/service/CartInfoService.java
+++ src/main/java/org/choongang/cart/service/CartInfoService.java
@@ -1,9 +1,73 @@
 package org.choongang.cart.service;
 
+import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.choongang.cart.constants.CartType;
+import org.choongang.cart.entities.CartInfo;
+import org.choongang.cart.entities.QCartInfo;
+import org.choongang.cart.repositories.CartInfoRepository;
+import org.choongang.commons.Utils;
+import org.choongang.member.MemberUtil;
+import org.choongang.product.constants.DiscountType;
+import org.choongang.product.entities.Product;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Service
 @RequiredArgsConstructor
 public class CartInfoService {
+
+    private final CartInfoRepository cartInfoRepository;
+    private final MemberUtil memberUtil;
+    private final Utils utils;
+
+    /**
+     * 장바구니에 담겨 있는 상품 목록
+     *
+     * @param mode : CART : 장바구니 노출 상품
+     *               DIRECT : 바로구매 상품
+     * @return
+     */
+    public List<CartInfo> getList(CartType mode) {
+        QCartInfo cartInfo = QCartInfo.cartInfo;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(cartInfo.mode.eq(mode));
+
+        List<CartInfo> items = (List<CartInfo>)cartInfoRepository.findAll(andBuilder, Sort.by(asc("createdAt")));
+
+        return items;
+    }
+
+    /**
+     * 장바구니 종합 데이터
+     *
+     * @param mode
+     * @return
+     */
+    public CartData getCartInfo(CartType mode) {
+
+        int totalPrice = 0, totalDiscount = 0, totalDeliveryPrice = 0, payPrice = 0;
+
+        List<CartInfo> items = getList(mode);
+        for (CartInfo item : items) {
+            Product product = item.getProduct();
+            int salePrice = product.getSalePrice(); // 상품 판매가
+            int ea = item.getEa(); // 구매 수량
+
+            totalPrice += salePrice * ea;
+
+            // 할인
+            DiscountType type = product.getDiscountType();
+            int discount = product.getDiscount();
+            if (type == DiscountType.PERCENT) { // % 할인
+                totalDiscount += Math.round(salePrice * ea * discount / 100.0);
+            } else { // 고정금액 할인
+                totalDiscount += discount;
+            }
+        }
+    }
 }
