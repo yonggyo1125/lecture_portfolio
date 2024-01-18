c594ab6fd68b2b745129947496c8023a09db1423
yonggyo1125 <yonggyo00@kakao.com>
2024-01-18 11:19:10 +0900

    장바구니 처리

--- src/main/java/org/choongang/cart/controllers/CartController.java
+++ src/main/java/org/choongang/cart/controllers/CartController.java
@@ -1,10 +1,14 @@
 package org.choongang.cart.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.cart.service.CartSaveService;
 import org.choongang.commons.ExceptionProcessor;
+import org.choongang.commons.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class CartController implements ExceptionProcessor {
 
+    private final CartSaveService cartSaveService;
+
+    @ModelAttribute("pageTitle")
+    public String getPageTitle() {
+        return Utils.getMessage("장바구니", "commons");
+    }
+
+    
     /**
      * 장바구니에 상품 등록
      *      mode : cart - 장바구니 페이지 노출 상품
@@ -21,9 +33,17 @@ public class CartController implements ExceptionProcessor {
      */
     @PostMapping("/save")
     public String save(RequestCart form, Errors errors, Model model) {
+        commonProcess("product", model);
 
-
+        cartSaveService.save(form);
 
         return "common/_execute_script";
+    }
+
+
+
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "list";
+
     }
 }
