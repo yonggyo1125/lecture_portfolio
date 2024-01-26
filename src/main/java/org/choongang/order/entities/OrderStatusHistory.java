6a8d797bcbc97ac623d1b89dd1653604876c1d02
yonggyo1125 <yonggyo00@kakao.com>
2024-01-26 17:00:52 +0900

    4조

---
+++ src/main/java/org/choongang/order/entities/OrderStatusHistory.java
@@ -0,0 +1,22 @@
+package org.choongang.order.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatusHistory {
+    @Id
+    @GeneratedValue
+    private Long seq;
+
+
+}
